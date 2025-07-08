a62cb59cae9a93cae3cc17fe3760829b5f97070c
Muhammad Tayyab <haris.a.mannan@gmail.com>
2025-07-08 21:39:01 +0500

    Implemented Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Plus_Jakarta_Sans } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import ClientLayout from '@/components/ClientLayout'
 import './globals.css'
 
@@ -34,6 +35,7 @@ export default function RootLayout({
         <ClientLayout>
           {children}
           <Analytics />
+          <SpeedInsights/>
         </ClientLayout>
       </body>
     </html>
